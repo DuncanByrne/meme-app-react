6f16258687eb0ef6b05311c029edad07c61b4373
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-11-22 14:41:37 -0500

    added a generate button

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -6,16 +6,31 @@ export const Meme = () =>{
   const [memes, setMemes] = useState([])
   const [memeIndex, setMemeIndex ] = useState(0);
 
-useEffect(() => {
-  fetch('https://api.imgflip.com/get_memes').then(res => res.json().then(res =>{
-    const memes = res.data.memes;
-    setMemes(memes);
+  // this function shuffles the order that the memes appear in
+  const shuffleMemes = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
 
-  }))
-});
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes').then(res => {
+      res.json().then(res => {
+        const _memes = res.data.memes;
+        shuffleMemes(_memes);
+        setMemes(_memes);
+      });
+    });
+  }, []);
   return(
   memes.length ? 
   <div className={styles.container}>
+    {/* generate button */}
+    <button onClick={() => console.log('generate')} className={styles.generate}>Generate</button>
+    {/* skip to another meme */}
     <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip}>Skip</button>
     <img src={memes[memeIndex].url} alt='meme'/> 
   </div>: 
